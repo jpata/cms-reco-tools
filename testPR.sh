c5db9e8c1c7c654876d9029cf9ab4e920863c029
Joosep Pata <joosep.pata@gmail.com>
2020-09-23 16:04:38 +0300

    update test script

--- testPR.sh
+++ testPR.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 set -e
 
-CMSSW=CMSSW_11_2_X_2020-08-03-2300
-TAG=guitargeek:PFElecTkProducer_1
-PR=30992
+CMSSW=CMSSW_11_2_X_2020-09-21-1100
+TAG=OzAmram:CR_local_error_bugfix
+PR=31469_2
+WORKFLOW=10859.0
+THREADS=8
+NUMEVENTS=100
 
-cd ~/work/reco
+cd ~/reco
 mkdir -p $PR
 cd $PR
 
-scram p -n orig CMSSW $CMSSW
+
+#run the PR
 scram p -n new CMSSW $CMSSW
 cd new
-
 eval `scramv1 runtime -sh`
 git cms-merge-topic --unsafe $TAG
 git cms-checkdeps -a >& dep.log 
-scram b -j 8 >& abuild.log 
-~/tools/buildQA.sh
+scram b -j $THREADS >& abuild.log 
+runTheMatrix.py -l $WORKFLOW --command="-n $NUMEVENTS --nThreads $THREADS"
+
+#run the reference
+cd ..
+scram p -n orig CMSSW $CMSSW
+cd orig
+eval `scramv1 runtime -sh`
+runTheMatrix.py -l $WORKFLOW --command="-n $NUMEVENTS --nThreads $THREADS"
