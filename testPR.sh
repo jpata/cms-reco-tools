f9c93a5bada5f28764c402a975cbaef7659dbe17
Joosep Pata <joosep.pata@gmail.com>
2020-09-23 18:00:55 +0300

    uncomment

--- testPR.sh
+++ testPR.sh
@@ -12,29 +12,29 @@ cd ~/reco
 mkdir -p $PR
 cd $PR
 
-##run the reference
-#scram p -n orig CMSSW $CMSSW
-#cd orig
-#eval `scramv1 runtime -sh`
-#runTheMatrix.py -l $WORKFLOW --command="-n $NUMEVENTS --nThreads $THREADS"
-#cd ..
-#
-##get the PR, compile
-#scram p -n new CMSSW $CMSSW
-#cd new
-#eval `scramv1 runtime -sh`
-#git cms-merge-topic --unsafe $TAG
-#git cms-checkdeps -a >& dep.log 
-#scram b -j $THREADS >& abuild.log
-#
-##copy the reference workflow
-#cp -R ../orig/$WORKFLOW_* ./
-#
-##rerun only RECO part
-#cd $WORKFLOW
-#cmsRun step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_VALIDATION_DQM.py > step4.log
-#cmsRun step4_HARVESTING.py >& step4.log
-#cd ../..
+#run the reference
+scram p -n orig CMSSW $CMSSW
+cd orig
+eval `scramv1 runtime -sh`
+runTheMatrix.py -l $WORKFLOW --command="-n $NUMEVENTS --nThreads $THREADS"
+cd ..
+
+#get the PR, compile
+scram p -n new CMSSW $CMSSW
+cd new
+eval `scramv1 runtime -sh`
+git cms-merge-topic --unsafe $TAG
+git cms-checkdeps -a >& dep.log 
+scram b -j $THREADS >& abuild.log
+
+#copy the reference workflow
+cp -R ../orig/$WORKFLOW_* ./
+
+#rerun only RECO part
+cd $WORKFLOW
+cmsRun step3_RAW2DIGI_L1Reco_RECO_RECOSIM_EI_PAT_VALIDATION_DQM.py > step4.log
+cmsRun step4_HARVESTING.py >& step4.log
+cd ../..
  
 #run JR comparison
 mkdir comparisonJR
