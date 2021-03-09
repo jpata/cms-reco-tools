b5fd0961b684378516f1ef8338db397465d65338
Joosep Pata <joosep.pata@gmail.com>
2021-03-09 18:03:28 +0200

    up

--- runIgprof.sh
+++ runIgprof.sh
@@ -8,8 +8,8 @@ python ~/tools/fix-igprof-sql.py ig.txt > ig_fixed.txt
 sqlite3 ig_cpu.sql3 < ig_fixed.txt
 
 #add --customise Validation/Performance/IgProfInfo.customise
-#igprof -mp -d -o ig.gz -t cmsRunGlibC cmsRunGlibC $pset 
-#igprof-analyse --sqlite -v --demangle --gdb -r MEM_LIVE IgProf.49.gz > ig.txt
-#python ~/tools/fix-igprof-sql.py ig.txt > ig_fixed.txt
-#sqlite3 ig_mem.sql3 < ig_fixed.txt
+igprof -mp -d -o ig.gz -t cmsRunGlibC cmsRunGlibC $pset 
+igprof-analyse --sqlite -v --demangle --gdb -r MEM_LIVE IgProf.99.gz > ig.txt
+python ~/tools/fix-igprof-sql.py ig.txt > ig_fixed.txt
+sqlite3 ig_mem.sql3 < ig_fixed.txt
 
--- testPR.sh
+++ testPR.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-CMSSW=CMSSW_11_2_X_2020-11-29-2300
-TAG=vberta:CMSSW_11_2_X_2020-11-18_DeepCoreTechnical
-PR=32222
+CMSSW=CMSSW_11_3_X_2021-01-28-2300
+TAG=hatakeyamak:SwitchFromSimPFToPFTICL
+PR=32766
 WORKFLOW=4.53
 THREADS=1
 NUMEVENTS=100
@@ -30,33 +30,33 @@ git cms-merge-topic --unsafe $TAG
 git cms-checkdeps -a >& dep.log 
 scram b -j $THREADS >& abuild.log
 
-#~/tools/extendedTests2021.sh
+~/tools/extendedTests2021.sh
 
-##copy the reference workflow
-#cp -R ../orig/$WORKFLOW_* ./
+#copy the reference workflow
+cp -R ../orig/$WORKFLOW_* ./
 
-#rerun only RECO part
-#cd $WORKFLOW_*
-#cmsRun step3_*.py >& step3_*.log
-#cmsRun step4_*.py >& step4_*.log
-#cd ../..
- 
-##run JR comparison (precompiled using validate_main.cpp && Makefile)
-#mkdir comparisonJR
-#cd comparisonJR
-#~/tools/validate ../new/$WORKFLOW_*/step3.root ../orig/$WORKFLOW_*/step3.root all > log
+rerun only RECO part
+cd $WORKFLOW_*
+cmsRun step3_*.py >& step3_*.log
+cmsRun step4_*.py >& step4_*.log
+cd ../..
 
-##run DQM comparison (multithreaded plots)
-#cd ..
-#mkdir comparisonDQM
-#cd comparisonDQM
-#mkdir plots
-#python ~/tools/validateJP.py --fn-new ../new/$WORKFLOW_*/DQM_*.root --fn-ref ../orig/$WORKFLOW_*/DQM*.root -j $THREADS \
-#  --require CorrOverGen --require GenMETTrue > log
-#
-##run an event-by-event diff using FWLite
-#cd ..
-#mkdir comparisonFWLite
-#cd comparisonFWLite
-#python ~/tools/fwlite_diff.py ../new/$WORKFLOW_*/step3.root > new.txt
-#python ~/tools/fwlite_diff.py ../orig/$WORKFLOW_*/step3.root > orig.txt 
+#run JR comparison (precompiled using validate_main.cpp && Makefile)
+mkdir comparisonJR
+cd comparisonJR
+~/tools/validate ../new/$WORKFLOW_*/step3.root ../orig/$WORKFLOW_*/step3.root all > log
+
+#run DQM comparison (multithreaded plots)
+cd ..
+mkdir comparisonDQM
+cd comparisonDQM
+mkdir plots
+python ~/tools/validateJP.py --fn-new ../new/$WORKFLOW_*/DQM_*.root --fn-ref ../orig/$WORKFLOW_*/DQM*.root -j $THREADS \
+  --require CorrOverGen --require GenMETTrue > log
+
+#run an event-by-event diff using FWLite
+cd ..
+mkdir comparisonFWLite
+cd comparisonFWLite
+python ~/tools/fwlite_diff.py ../new/$WORKFLOW_*/step3.root > new.txt
+python ~/tools/fwlite_diff.py ../orig/$WORKFLOW_*/step3.root > orig.txt 
